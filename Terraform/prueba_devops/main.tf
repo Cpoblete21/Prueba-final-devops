e0cc5a162cb3636a4760df9b54b89bb5bf8ca363
Cpoblete21 <106352215+Cpoblete21@users.noreply.github.com>
2025-01-02 16:50:38 -0300

    Update main.tf

--- Terraform/prueba_devops/main.tf
+++ Terraform/prueba_devops/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region= "us-east-1"
-    access_key = "AKIAVFIWI7D462ACDUWP"
-    secret_key = "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"
+
 }
 module "vpc" {
   source = "./modules/vpc"
